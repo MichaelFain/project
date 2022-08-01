d2224417d536083d576a07c8d210538304955452
Michael <mishafff@gmail.com>
2022-08-01 19:02:26 +0300

    ffff

--- Javascript/script2.js
+++ Javascript/script2.js
@@ -1,12 +1,28 @@
-"use strict";
+// "use strict";
 
-//let num = 50;
-
-// while (num <= 55) {
+// let num = 5;
+//
+//  while (num <= 10) {
 //     console.log(num);
-//     num++;
-// }
+//      num++;
+//  }
 
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+// let i = 2;
+// while (i < 16) {
+//     i++;
+//     if (i % 2 ===0) {
+//         continue;
+//     } else {
+//     console.log(i)
+//         }
+//}
 // do {
 //     console.log(num);
 //     num++;
@@ -18,14 +34,22 @@
 //     num++;
 // }
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        break;
-    }
-    console.log(i);
+// for (let i = 20; i > 10; i--) {
+//     if (i === 13) {
+//         break;
+//     }
+//     console.log(i);
+//
+// }
 
-}
-
+// function firstTask() {
+//     let num <= 5;
+//     while (num <= 10) {
+//       console.log( num);
+//       num++;
+//     }
+//
+// }
 // for (let i = 1; i < 10; i++) {
 //     if (i === 6) {
 //         continue;
@@ -33,3 +57,18 @@ for (let i = 1; i < 10; i++) {
 //     console.log(i);
 //
 // }
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+    for (let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+    console.log(arrayOfNumbers);
+    return arrayOfNumbers;
+}
